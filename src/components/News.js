3e98be345c7cce94798bcf42b54485b071e6ac56
Usama Rasheed <usamara760@gmail.com>
2024-08-13 02:47:15 +0500

    Added infinte scroll

--- src/components/News.js
+++ src/components/News.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner2';
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export class News extends Component{
 
@@ -64,30 +65,54 @@ export class News extends Component{
             );
         }
     }
+    fetchMoreData = async () => {
+        
+        this.setState({page: this.state.page + 1});
+
+        const { category } = this.props;
+        const { page } = this.state;
+        const url = `https://newsapi.org/v2/top-headlines?q=${category}&apiKey=8a333109df7045e4b0d4dbf0b1b9d221&page=${page}&pageSize=${this.props.pageNo}`;
+        const data = await fetch(url);
+        const parsedData = await data.json();
+        this.setState({
+            articles: this.state.articles.concat(parsedData.articles || []),
+            totalResults: parsedData.totalResults
+        });
+        
+      };
 
     render() {
         return (
-            <div className='container my-4'>
+            <>
                 <h1 className="text-center" style={{ margin: '40px' }}>NewsFox - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
                 {this.state.loading && <Spinner />}
-                <div className="row mt-4">
-                    {!this.state.loading && this.state.articles.map((element) => {
-                        return <div className="col-md-4" key={element.url}>
-                            <NewsItem
-                                title={element.title ? element.title.slice(0, 45) : ""}
-                                description={element.description ? element.description.slice(0, 88) : ""}
-                                imgUrl={element.urlToImage}
-                                newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}
-                            />
+                <InfiniteScroll
+                    dataLength={this.state.articles.length}
+                    next={this.fetchMoreData}
+                    hasMore={this.state.articles.length !== this.state.totalResults}
+                    loader={<Spinner/>}
+                >
+                    <div className='container'>
+                        <div className="row mt-4">
+                            {this.state.articles.map((element) => {
+                                return <div className="col-md-4" key={element.url}>
+                                    <NewsItem
+                                        title={element.title ? element.title.slice(0, 45) : ""}
+                                        description={element.description ? element.description.slice(0, 88) : ""}
+                                        imgUrl={element.urlToImage}
+                                        newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}
+                                    />
+                                </div>
+                            })}
                         </div>
-                    })}
-                </div>
+                    </div>
+                </InfiniteScroll>
 
                 <div className='container d-flex justify-content-between'>
                     <button disabled={this.state.page <= 1} type="button" className="btn btn-success" onClick={this.handlePrevClick}>&larr; Prev</button>
                     <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageNo)} type="button" className="btn btn-success" onClick={this.handleNextClick}>Next &rarr;</button>
                 </div>
-            </div>
+            </>
         )
     }
 }
--- src/components/Spinner2.js
+++ src/components/Spinner2.js
@@ -5,7 +5,7 @@ export class Spinner extends Component {
   render() {
     return (
       <div className='text-center'>
-        <img src={loader} alt='loader..' style={{ width: '50px', height: '50px' }}></img>
+        <img className='my-4' src={loader} alt='loader..' style={{ width: '50px', height: '50px' }}></img>
       </div>
     )
   }
